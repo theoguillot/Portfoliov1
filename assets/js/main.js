82ea473cbc826a90c167271dd6142e1ee9b3983c
theoguillot <theo.guillot06@gmail.com>
2022-03-11 10:41:04 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,22 +94,20 @@ modalCloses.forEach((modalClose, i)=>{
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
 const swiper = new Swiper('.portfolio__container', {  
+    cssMode: true,
     loop: true,
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination'
+      el: '.swiper-pagination',
+      clickable: true,
+
     },
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    }
   });
   
   
